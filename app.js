02c42f26d83bbc805f547c4b2cd21a7406d7a95c
Thomas <thomas86.cornu@gmail.com>
2024-11-23 12:40:31 +0100

    adding configuration cors

--- app.js
+++ app.js
@@ -11,8 +11,11 @@ var tweetsRouter = require('./routes/tweets');
 var app = express();
 
 const cors = require('cors');
-app.use(cors());
-
+app.use(cors({
+    origin: 'https://frontend-hackatweet.vercel.app', 
+    credentials: true, }  
+));
+app.options('*', cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
--- models/connection.js
+++ models/connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 mongoose.set('strictQuery', false);
 const connectionString = process.env.CONNECTION_STRING;
 
-mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
+mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true, connectTimeoutMS: 2000 })
   .then(() => console.log('Database connected'))
   .catch(error => console.error(error));
 console.log("Server running on port 3000 ...");
